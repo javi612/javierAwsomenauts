2c7d2f8f5f68f5d49725b8632a0c4548d75900d7
javier alvarez <javi_612@yahoo.com>
2015-04-22 20:55:14 -0700

    done

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -1,4 +1,4 @@
-game.GameManager = Object.extend({
+game.GameTimerManager = Object.extend({
     init: function(x, y, settings) {
         this.now = new Date().getTime();
         this.lastCreep = new Date().getTime();
@@ -6,25 +6,40 @@ game.GameManager = Object.extend({
         this.alwaysUpdate = true;
     },
     update: function() {
-        this.now = new Date().getTime();
-        
-        if(game.data.player.dead){
-            me.game.world.removeChild(game.data.player);
-            me.state.current().resetPlayer(10, 0);
-        }
+        this.now = new Date().getTime();        
+        this.goldTimerCheck();
+        this.creepTimerCheck();
 
-        if (Math.round(this.now / 1000) %20 === 0 && (this.now - this.lastCreep >= 1000)) {
+
+        return true;
+    },
+    
+    goldTimerCheck: function(){
+       if (Math.round(this.now / 1000) %20 === 0 && (this.now - this.lastCreep >= 1000)) {
             game.data.gold  += 1;
             console.log("Current gold: " + game.data.gold);
-        }
-
-        if (Math.round(this.now / 1000) %10 === 0 && (this.now - this.lastCreep >= 1000)) {
+        } 
+    }
+    
+    creepTimerCheck: function(){
+        if (Math.round(this.now/1000) %10 === 0 && (this.now - this.lastCreep >= 1000)) {
             this.lastCreep = this.now;
             var creepe = me.pool.pull("EnemyCreep", 1000, 0, {});
             me.game.world.addChild(creepe, 5);
         }
+    } 
+});
 
-        return true;
+game.HeroDeathManager = Object.extend( {
+    init: function(x, y, settings){
+        this.alwaysUpdate = true;
+    },
+    
+    update: function(){
+        if(game.data.player.dead){
+            me.game.world.removeChild(game.data.player);
+            me.state.current().resetPlayer(10, 0);
+        }
     }
 });
 
--- js/game.js
+++ js/game.js
@@ -20,7 +20,8 @@ var game = {
                 enemyCreepAttackTimer: 1000,
                 playerMoveSpeed: 10,
                 creepMoveSpeed: 5,
-                gameManager: "",
+                gameTimerManager: "",
+                heroDeathManager: "",
                 player: "",
                 exp: 0,
                 gold: 0,
@@ -66,7 +67,8 @@ var game = {
                 me.pool.register("PlayerBase", game.PlayerBaseEntity);
                 me.pool.register("EnemyBase", game.EnemyBaseEntity);
                 me.pool.register("EnemyCreep", game.EnemyCreep, true);
-                me.pool.register("GameManager", game.GameManager);
+                me.pool.register("GameTimerManager", game.GameTimerManager);
+                me.pool.register("HeroDeathManager", game.HeroDeathManager);
                 
             
 		me.state.set(me.state.MENU, new game.TitleScreen());
