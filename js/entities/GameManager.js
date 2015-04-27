db5f7519bfe24303dcc3f0b1034d7b52882ffb12
javier alvarez <javi_612@yahoo.com>
2015-04-27 14:19:43 -0700

    catchin up

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -82,10 +82,79 @@ game.SpendGold = Object.extend({
         this.lastBuy = new Date().getTime();
         this.paused = false;
         this.alwaysUpdate = true; 
+        this.updateWhenPaused = true;
+        this.buying = false;
     },
     
     update: function(){
+        this.now = new Date().getTime();
+        
+        if(me.input.isKeyPressed("buy") && this.now-this.lastBuy >= 1000){
+            this.lastBuy = this.now;
+            if(!this.buying){
+                this.startBuying();
+            }else{
+                this.stopBuying();
+            }
+            
+        }
+        
         return false;
+    },
+    
+    startBuying: function(){
+        this.buying = true;
+        me.state.pause(me.state.PlAY);
+        game.data.pausePos = me.game.viewport.localToWorld(0, 0);
+        game.data.buyscreen = new me.Sprite(game.data.pausePos.x, game.data.pausePos.y, me.loader.gateImage('gold-screen'));
+        game.data.buyscreen.updateWhenPaused = true;
+        game.data.buyscreen.setOPacity(0.8);
+        me.game.world.addChild(game.data.buyscreen, 34);
+        game.data.player.body.setVelocity(0, 0);
+        me.input.bindKey(me.input.KEY.F1, "F1" , true);
+        me.input.bindKey(me.input.KEY.F2, "F2" , true);
+        me.input.bindKey(me.input.KEY.F3, "F3" , true);
+        me.input.bindKey(me.input.KEY.F4, "F4" , true);
+        me.input.bindKey(me.input.KEY.F5, "F5" , true);
+        me.input.bindKey(me.input.KEY.F6, "F6" , true);
+        this.setBuyText();
+    },
+    
+    setBuyText: function(){
+       game.data.buytext = new (me.Renderable.extend({
+            init: function(){
+                this._super(me.Renderable, 'init', [game.data.pausePos.y, 10, 300, 50]);
+                this.font = new me.Font("Ariel", 26, "white");
+                this.updateWhenPaused = true;
+                this.alwaysUpdate = true;
+            },
+            
+            draw: function(renderer){
+                this.font.draw(renderer.getContext(), "PRESS F1-F6 TO BUY, B TO EXIT. Current Gold: " + game.data.gold, this.pos.x, this.pos.y);     
+                this.font.draw(renderer.getContext(), "PRESS F1-F6 TO BUY, B TO EXIT", this.pos.x, this.pos.y + 40);  
+                this.font.draw(renderer.getContext(), "PRESS F1-F6 TO BUY, B TO EXIT", this.pos.x, this.pos.y + 80);     
+                this.font.draw(renderer.getContext(), "PRESS F1-F6 TO BUY, B TO EXIT", this.pos.x, this.pos.y + 120);     
+                this.font.draw(renderer.getContext(), "PRESS F1-F6 TO BUY, B TO EXIT", this.pos.x, this.pos.y + 160);     
+                this.font.draw(renderer.getContext(), "PRESS F1-F6 TO BUY, B TO EXIT", this.pos.x, this.pos.y + 200);     
+                this.font.draw(renderer.getContext(), "PRESS F1-F6 TO BUY, B TO EXIT", this.pos.x, this.pos.y + 240);     
+            }      
+           
+        }));
+    me.game.world.addChild(game.data.buytext, 35);
+    },
+    
+    stopBuying: function(){
+        this.buying = false;
+        me.state.resume(me.state.PlAY);
+        game.data.player.body.setVelocity(game.data.playerMoveSpeed, 20);
+        me.game.world.removeChild(game.data.buyscreen);
+        me.input.unbindKey(me.input.KEY.F1, "F1" , true);
+        me.input.unbindKey(me.input.KEY.F2, "F2" , true);
+        me.input.unbindKey(me.input.KEY.F3, "F3" , true);
+        me.input.unbindKey(me.input.KEY.F4, "F4" , true);
+        me.input.unbindKey(me.input.KEY.F5, "F5" , true);
+        me.input.unbindKey(me.input.KEY.F6, "F6" , true);
+        ma.game.world.removeChild(game.data.buytext);
     }
     
 });
